23a8cdf4d1a4aa9d8505e73865f51d98c28897d3
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-04-25 19:01:57 -0400

    Improve star

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ effect.domElement.style.backgroundColor = "black";
 effect.domElement.style.position = "fixed";
 effect.domElement.style.top = "0";
 effect.domElement.style.left = "0";
-effect.domElement.style.opacity = "0.1";
+effect.domElement.style.opacity = "0.15";
 effect.domElement.style.zIndex = "-1";
 
 const loader = new GLTFLoader();
@@ -31,12 +31,19 @@ const loader = new GLTFLoader();
 let model = null;
 loader.load("star.glb", function (gltf) {
   model = gltf.scene.children[0];
-  model.material = new THREE.MeshBasicMaterial({
+  model.material = new THREE.MeshLambertMaterial({
     color: 0x00ff00,
   });
   model.scale.set(0.5, 0.5, 0.5);
   scene.add(model);
 });
+
+const pointLight = new THREE.PointLight(0xffffff, 100);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+scene.add(ambientLight);
 
 function animate() {
   requestAnimationFrame(animate);
